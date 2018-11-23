78c77f2391e342c307f690f67afafa4db9e16628
Chillen <josh.jenkin@live.com>
2018-11-23 16:49:56 -0700

    got radio buttons, need to make quiz work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,65 @@
 // Create variables; answers, questions array?, timer
 var questions = [
     {
-    question: "What year was The Shawshank Redemption released?", 
-    answers: {
-        a: "1995",
-        b: "1994",
-        c: "1999",
-        d: "1989"
+        question: "What year was The Shawshank Redemption released?",
+        answers: {
+            a: "1995",
+            b: "1994",
+            c: "1999",
+            d: "1989"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-},
-{
-    question: "What is the highest grossing film of all time?", 
-    answers: {
-        a: "Titanic",
-        b: "Avatar",
-        c: "The Avengers",
-        d: "Skyfall"
+    {
+        question: "What is the highest grossing film of all time?",
+        answers: {
+            a: "Titanic",
+            b: "Avatar",
+            c: "The Avengers",
+            d: "Skyfall"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-},
-{
-    question: "Which actor has the most Oscar Nominations?", 
-    answers: {
-        a: "Denzel Washington",
-        b: "Jeff Bridges",
-        c: "Meryl Streep",
-        d: "Katharine Hepburn"
+    {
+        question: "Which actor has the most Oscar Nominations?",
+        answers: {
+            a: "Denzel Washington",
+            b: "Jeff Bridges",
+            c: "Meryl Streep",
+            d: "Katharine Hepburn"
+        },
+        correctAnswer: "c"
     },
-    correctAnswer: "c"
-},
-{
-    question: "Who directed The Fantastic Mr. Fox?", 
-    answers: {
-        a: "Steven Spielberg",
-        b: "Quentin Tarantino",
-        c: "Ron Howard",
-        d: "Wes Anderson"
+    {
+        question: "Who directed The Fantastic Mr. Fox?",
+        answers: {
+            a: "Steven Spielberg",
+            b: "Quentin Tarantino",
+            c: "Ron Howard",
+            d: "Wes Anderson"
+        },
+        correctAnswer: "d"
     },
-    correctAnswer: "d"
-},
-{
-    question: "Who was the lead in The Shining?", 
-    answers: {
-        a: "Jack Nickolson",
-        b: "Robert De Niro",
-        c: "Al Pacino",
-        d: "Clint Eastwood"
+    {
+        question: "Who was the lead in The Shining?",
+        answers: {
+            a: "Jack Nickolson",
+            b: "Robert De Niro",
+            c: "Al Pacino",
+            d: "Clint Eastwood"
+        },
+        correctAnswer: "a"
     },
-    correctAnswer: "a"
-},
-{
-    question: "Who was the first director to break the \"Fourth wall\"?", 
-    answers: {
-        a: "Martin Scorsese",
-        b: "Woody Allen",
-        c: "Ron Howard",
-        d: "Quentin Tarantino"
+    {
+        question: "Who was the first director to break the \"Fourth wall\"?",
+        answers: {
+            a: "Martin Scorsese",
+            b: "Woody Allen",
+            c: "Ron Howard",
+            d: "Quentin Tarantino"
+        },
+        correctAnswer: "b"
     },
-    correctAnswer: "b"
-},
 ];
 var timer = 60;
 var intervalID;
@@ -72,16 +72,16 @@ function run() {
     intervalID = setInterval(decrement, 1000);
 }
 // grab id for time, set timer function, link to end game function and display answers/score.
-function decrement(){
+function decrement() {
     timer--;
-$("#time-rem").html("<h1>Time Remaining: " + timer + "</h1>");
-if (timer === 0) {
-    timesUp();
-}
+    $("#time-rem").html("<h1>Time Remaining: " + timer + "</h1>");
+    if (timer === 0) {
+        timesUp();
+    }
 }
 
 // grab ID's for Start Game, hide everything else until start is pressed, hide start after pressed
-$(document).ready(function(){
+$(document).ready(function () {
     $("#submit").hide();
     // $("#time-rem").hide();
 });
@@ -90,18 +90,59 @@ $("#start-game").click(quizBody);
 
 function quizBody() {
     run();
-    
+
     $("#start-game").hide();
     $("#submit").show();
     questionsAndAnswers();
-// alert("hello");
+    // alert("hello");
 };
 
-    // function for questions and answers, add radio buttons for answers.
-function questionsAndAnswers () {
+// function for questions and answers, add radio buttons for answers.
+function questionsAndAnswers() {
     // var output = [];
     // questions.forEach()
-    $("#questions").append("questions.question[1], questions.answers[1]");
+    $("#questions").append(
+        "<div><h3>What year was The Shawshank Redemption released?</h3><div><br>",
+        "<div>" + "<input type='radio' name='q1'>" + questions[1].answers[1] + "</input> " +
+        "<input type='radio' name='q1'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q1'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q1'>" + questions.answers + "</input> " + "</div><br>",
+
+
+        "<div><h3>Who was the first director to break the \"Fourth wall\"?</h3><div><br>",
+        "<div>" + "<input type='radio' name='q1'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q2'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q2'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q2'>" + questions.answers + "</input> " + "</div><br>",
+
+
+        "<div><h3>What year was The Shawshank Redemption released?</h3><div><br>",
+        "<div>" + "<input type='radio' name='q1'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q3'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q3'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q3'>" + questions.answers + "</input> " + "</div><br>",
+
+
+        "<div><h3>What year was The Shawshank Redemption released?</h3><div><br>",
+        "<div>" + "<input type='radio' name='q1'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q4'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q4'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q4'>" + questions.answers + "</input> " + "</div><br>",
+
+
+        "<div><h3>What year was The Shawshank Redemption released?</h3><div><br>",
+        "<div>" + "<input type='radio' name='q1'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q5'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q5'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q5'>" + questions.answers + "</input> " + "</div><br>",
+
+
+        "<div><h3>What year was The Shawshank Redemption released?</h3><div><br>",
+        "<div>" + "<input type='radio' name='q1'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q6'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q6'>" + questions.answers + "</input> " +
+        "<input type='radio' name='q6'>" + questions.answers + "</input> " + "</div><br>",
+    );
 }
 // for reference
 // var questions = [
@@ -118,7 +159,7 @@ function questionsAndAnswers () {
 
 
 // function for clicking submit button linked to game end function
-$("#submit").click(function(){
+$("#submit").click(function () {
     timesUp();
 });
 
